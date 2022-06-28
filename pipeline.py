83c6f959a38466cdd73ce35ce0caf4e9861d84b8
César Salcedo <cesar.salcedo@utec.edu.pe>
2022-06-28 07:56:29 -0500

    Removed unnecessary code

--- pipeline.py
+++ pipeline.py
@@ -2,11 +2,8 @@ import kfp
 from kfp import dsl
 import kfp.components as comp
 
-@dsl.pipeline(name='ejemplo-simple')
+@dsl.pipeline(name='xor-')
 def simple_pipeline(
-        min:int=0,
-        max:int=100,
-        num:int=5,
         train_size:int=100,
         test_size:int=10,
         dims:int=2
@@ -17,32 +14,7 @@ def simple_pipeline(
 
     with dsl.Condition(dims == 2):
         sample_scatter = comp.load_component_from_file('components/sample_scatter.yaml')
-        op_sample_scatter = sample_scatter(input_dir = op_xor_sampler.output)
-
-    generate_data = comp.load_component_from_file('components/generator.yaml')
-    op_gen = generate_data(min=min, max=max, num=num)
-
-    with dsl.ParallelFor(op_gen.output) as e:
-        dsl.ContainerOp(
-            name='prob-de-elemento',
-            image='library/bash:4.4.23',
-            command=['sh', '-c'],
-            arguments=['echo Probabilidad de obtener {} de la distribucion: 0.01'.format(e)],
-        )
-
-    dsl.ContainerOp(
-        name='promedio',
-        image='python:alpine3.7',
-        command=['sh', '-c'],
-        arguments=['echo Promedio de los numeros: && python -c "print(sum({0})/len({0}))"'.format(op_gen.output)],
-    )
-
-    dsl.ContainerOp(
-        name='suma',
-        image='python:alpine3.7',
-        command=['sh', '-c'],
-        arguments=['echo Suma de los numeros: && python -c "print(sum({}))"'.format(op_gen.output)],
-    )
+        op_sample_scatter = sample_scatter(input_dir=op_xor_sampler.output)
 
 
 if __name__ == '__main__':
