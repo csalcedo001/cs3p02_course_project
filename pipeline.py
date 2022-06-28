49001303a2a43ce3f7ff36bb1cf2a80c09ffd872
César Salcedo <cesar.salcedo@utec.edu.pe>
2022-06-28 07:54:34 -0500

    Added conditional for scatter plot, since plots only work for dims=2

--- pipeline.py
+++ pipeline.py
@@ -15,8 +15,9 @@ def simple_pipeline(
     xor_sampler = comp.load_component_from_file('components/xor_sampler.yaml')
     op_xor_sampler = xor_sampler(dims=dims, train_size=train_size, test_size=test_size)
 
-    sample_scatter = comp.load_component_from_file('components/sample_scatter.yaml')
-    op_sample_scatter = sample_scatter(input_dir = op_xor_sampler.output)
+    with dsl.Condition(dims == 2):
+        sample_scatter = comp.load_component_from_file('components/sample_scatter.yaml')
+        op_sample_scatter = sample_scatter(input_dir = op_xor_sampler.output)
 
     generate_data = comp.load_component_from_file('components/generator.yaml')
     op_gen = generate_data(min=min, max=max, num=num)
